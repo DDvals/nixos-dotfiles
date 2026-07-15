03bac6316a8c835db86837cccab87ae60a51ebef
DDvals <d.valsecchi88@gmail.com>
2026-07-15 19:09:17 +0200

    dms in niri

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,20 +1,13 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    fuzzel
-    swaylock
-    mako
-    #waybar
-    swayidle
-    swaybg
     xwayland-satellite
   ];
 
   programs.niri.enable = true;
-  programs.waybar.enable = true;
+  programs.dms-shell.enable = true;
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.swaylock = { };
   xdg.portal = {
     enable = true;
     wlr.enable = true;
